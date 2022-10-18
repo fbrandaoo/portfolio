1e1723ddd62ff7d3bd5005779d2cf6f6d1811c0f
Fernando <fbrandao1010@gmail.com>
2022-10-18 12:26:37 -0300

    Add sun/moon icons on darkmode button + font-family: fontawesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 let chk = document.querySelector('#darkmode-toggle')
+let sun = document.querySelector('.fa-sun')
+let moon = document.querySelector('.fa-moon')
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
+    
+    if(document.body.classList.contains('dark')) {
+        sun.style.opacity = 100
+        moon.style.opacity = 0
+    } else{
+        sun.style.opacity = 0
+        moon.style.opacity = 100
+    }
 })
